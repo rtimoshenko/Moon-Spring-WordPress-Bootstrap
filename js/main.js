1c530a295410db76babf9b996079b8ab1c8b7b07
Ronald Timoshenko <ronald.timoshenko@gmail.com>
2013-08-15 10:43:05 -0500

    Chained toggle methods, removed Cycle invocation.

--- js/main.js
+++ js/main.js
@@ -2,28 +2,18 @@
 $(function()
 {
 	// Gravity forms plugin applies class to field wrapper instead of field
-	$("input.toggle, textarea.toggle, .toggle input, .toggle textarea").focus(function(e)
-	{
-		if (!$(this).attr("data-default"))
-			$(this).attr("data-default", $(this).val());
-		
-		if ($(this).val() == $(this).attr("data-default"))
-			$(this).val("");
-	});
-	
-	$("input.toggle, textarea.toggle, .toggle input, .toggle textarea").blur(function(e)
-	{
-		if ($(this).val() == "")
-			$(this).val($(this).attr("data-default"));
-	});
-	
-	/*$('#mid .slides_container').cycle({
-		fx: 'fade',
-	    //prev: '#prev', 
-	    //next: '#next',
-	    //pager:  '#mid .pagination',
-		timeout: 6000,
-		containerResize: false,
-		slideResize: false
-	});*/
+	$("input.toggle, textarea.toggle, .toggle input, .toggle textarea")
+		.focus(function()
+		{
+			if (!$(this).attr("data-default"))
+				$(this).attr("data-default", $(this).val());
+			
+			if ($(this).val() == $(this).attr("data-default"))
+				$(this).val("");
+		})
+		.blur(function()
+		{
+			if ($(this).val() == "")
+				$(this).val($(this).attr("data-default"));
+		});
 });
